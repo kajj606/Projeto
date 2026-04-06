c3045ba135a2ea4b3b127c850457216aca79a91c
elemsm <munizvinicius439@gmail.com>
2026-04-06 13:12:20 -0300

    modified:   CSS/style.css 	new file:   IMAGENS/trabalho-agua2.jpg 	modified:   JAVASCRIPT/script.js     modified:   index.html

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,21 @@
+const cadastrar = document.getElementById('cadastre-se');
+const fomr = document.getElementById('my-form')
+const enviar = document.getElementById('enviar')
+const texto_agradecimento = document.getElementById('texto_agradecimento')
+const trabalhe_conosco = document.getElementById('trabalhe_conosco')
+cadastrar.addEventListener('click', function(){
+    if(fomr.style.display == 'none' || fomr.style.display == ''){
+        fomr.style.display = "block";
+        cadastrar.style.display = "none"
+    } else {
+        fomr.style.display = "none";
+    }
+});
+enviar.addEventListener('click', function(){
+    if(texto_agradecimento.style.display == 'none'){
+        texto_agradecimento.style.display = 'block'
+        fomr.style.display = 'none'
+        trabalhe_conosco.style.display = 'none'
+    }
+})
+
